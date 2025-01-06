9361d62f370e10aaeb531f2995b1e6ecaebf412e
18gen <ichihashigen@gmail.com>
2025-01-06 00:35:23 -0500

    heading modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,21 +10,27 @@ import {
 } from "@chakra-ui/react";
 import { Avatar } from "@/components/ui/avatar";
 import { useColorModeValue } from "@/components/ui/color-mode"
+import './globals.css';
+
 export default function Page() {
+  const backgroundColor = useColorModeValue("whiteAlpha.800", "whiteAlpha.200");
+  const headingClassName = useColorModeValue("text-shadow-lg", "");
+  const textClassName = useColorModeValue("text-shadow-sm", "");
+  
   return (
     <Container pt={8} maxW={{ base: "md", md: "2xl" }}>
-      <Box >
+      <Box>
         <Box
           borderRadius="lg"
           p={3}
           mb={5}
           textAlign="center"
-          background={useColorModeValue("whiteAlpha.800","whiteAlpha.200")}
+          background={backgroundColor}
           width="100%"
           css={{ backdropFilter: 'blur(10px)' }}
         >
           Hey👋, I study Computer Science at{" "}
-          <Text as="span" display={{ base: "inlaine", md: "none" }}>
+          <Text as="span" display={{ base: "inline", md: "none" }}>
             UWaterloo
           </Text>
           <Text as="span" display={{ base: "none", md: "inline" }}>
@@ -38,10 +44,15 @@ export default function Page() {
           justifyContent={{ md:"space-between"}}
         >
           <Box>
-            <Heading fontSize={{ base: "3xl", md: "4xl"}} fontWeight="bold" lineHeight="short">
-              Gen Ichihashi
-            </Heading>
-            <Text>Full-Stack Developer based in Japan</Text>
+          <Heading
+            fontSize={{ base: "3xl", md: "4xl" }}
+            fontWeight="bold"
+            lineHeight="short"
+            className={headingClassName}
+          >
+            Gen Ichihashi
+          </Heading>
+          <Text className={textClassName}>Full-Stack Developer based in Japan</Text>
           </Box>
           <Box mt={{ base: 4, md: 0 }} textAlign="center">
             <Avatar
