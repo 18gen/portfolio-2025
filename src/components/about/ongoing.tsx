74b06591f964a2168193f9345f9e87bce852ad75
18gen <ichihashigen@gmail.com>
2026-03-15 20:41:31 -0400

    update ongoing

--- src/components/about/ongoing.tsx
+++ src/components/about/ongoing.tsx
@@ -37,64 +37,7 @@ export default function Ongoing() {
                 fontWeight="bold"
                 asChild
               >
-                <NextLink href="/works">Summer Internship</NextLink>
-              </Link>
-            </Text>
-          </Card.Footer>
-        </Card.Root>
-
-        {/* Card 2 */}
-        <Card.Root width="260px" height="200px" size="md" variant="subtle">
-          <Card.Body>
-            <HStack mb="3" gap="3">
-              <Avatar src="works/geesehacks.jpg" name="Geesehacks" />
-              <Heading fontWeight="md" fontSize="md">
-                GeeseHacks
-              </Heading>
-            </HStack>
-            <Card.Description fontSize="sm">
-              <Strong color="fg">Director of 300+ Hackathon</Strong> at UW,
-              secured 22k funding and 600+ applied so far
-            </Card.Description>
-          </Card.Body>
-          <Card.Footer>
-            <Text fontSize="sm">
-              check out our{" "}
-              <Link
-                variant="underline"
-                href="https://geesehacks.com"
-                colorPalette="cyan"
-                fontWeight="bold"
-              >
-                website
-              </Link>
-            </Text>
-          </Card.Footer>
-        </Card.Root>
-
-        <Card.Root width="260px" height="200px" size="md" variant="subtle">
-          <Card.Body>
-            <HStack mb="3" gap="3">
-              <Avatar src="works/mics.jpg" name="MICS" />
-              <Heading fontWeight="md" fontSize="md">
-                Software Engineer
-              </Heading>
-            </HStack>
-            <Card.Description fontSize="sm">
-              Part-time developing a tool for real-time clinic performance
-              insights and decision support.
-            </Card.Description>
-          </Card.Body>
-          <Card.Footer>
-            <Text fontSize="sm">
-              check out my{" "}
-              <Link
-                variant="underline"
-                colorPalette="cyan"
-                fontWeight="bold"
-                asChild
-              >
-                <NextLink href="/works">experience</NextLink>
+                <NextLink href="/works">Internship</NextLink>
               </Link>
             </Text>
           </Card.Footer>
